a8fc9e5dd9b20ac9af02c3b2dd25377f15ba5ceb
akrnv25 <akrnv25@gmail.com>
2023-04-04 00:34:17 +0400

    added menu view; added add data view

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,37 @@
 const postService = require('./services/post.service');
+const manifest = require('./manifest.json');
 
-postService.sendRuntimeMessage('downloadAuctionPages').then(() => console.log('Success'));
+const menuView = document.getElementById('menu-view');
+const addDataView = document.getElementById('add-data-view');
+
+patchVersion();
+initMenuView();
+
+function initMenuView() {
+  menuView.classList.remove('hidden');
+  addDataView.classList.add('hidden');
+  const addDataButton = document.getElementById('add-data-button');
+  addDataButton.onclick = initAddDataView;
+  const downloadPagesButton = document.getElementById('download-pages-button');
+  downloadPagesButton.onclick = async () => {
+    await postService.sendRuntimeMessage('downloadAuctionPages');
+  };
+}
+
+function initAddDataView() {
+  addDataView.classList.remove('hidden');
+  menuView.classList.add('hidden');
+  const typeControl = document.getElementById('type-control');
+  const idControl = document.getElementById('ide-control');
+  const priceControl = document.getElementById('price-control');
+  const volumeControl = document.getElementById('volume-control');
+  const saveButton = document.getElementById('save-button');
+  const menuButton = document.getElementById('menu-button');
+  menuButton.onclick = initMenuView;
+}
+
+function patchVersion() {
+  const version = `Версия ${manifest.version}`;
+  const versionBox = document.getElementById('version-box');
+  versionBox.innerText = version;
+}
